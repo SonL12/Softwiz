b5036fdd1a577eeeae209bedce80f6994d3b3452
Son L <ahmadlekhareza@gmail.com>
2024-08-03 17:38:25 +0700

    Styling navbar with bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import './header.css'
 const Header = () => (
+    // <header>
+    // <div className="container">
+    //     <div className="logo">Softwiz</div>
+    //     <nav>
+    //     <ul>
+    //         <li><Link to="/">Home</Link></li>
+    //         <li><Link to="/services">Services</Link></li>
+    //         <li><Link to="/portfolio">Portfolio</Link></li>
+    //         <li><Link to="/about">About</Link></li>
+    //         <li><Link to="/contact">Contact</Link></li>
+    //     </ul>
+    //     </nav>
+    // </div>
+    // </header>
     <header>
-    <div className="container">
-        <div className="logo">Softwiz</div>
-        <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/portfolio">Portfolio</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-        </ul>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand d-flex align-items-center" to="/">Softwiz</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            <div className="collapse navbar-collapse margin" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/services">Services</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
         </nav>
-    </div>
     </header>
 );
 
